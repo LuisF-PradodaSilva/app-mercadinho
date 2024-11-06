941e1e4ec1ca71b4cbf716a8b634a3fd67a81b67
LuisFPradoSilva <luisfpradosilva.profissional@proton.me>
2024-11-06 19:57:28 -0300

    Inicializando o projeto

---
+++ src/main/java/com/mercadinho/appmercadinho/AppmercadinhoApplication.java
@@ -0,0 +1,13 @@
+package com.mercadinho.appmercadinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppmercadinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppmercadinhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercadinho/appmercadinho/AppmercadinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadinho.appmercadinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppmercadinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
