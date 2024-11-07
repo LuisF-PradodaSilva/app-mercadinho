d700f9d66984343d09dc5d37a746e53a4160749b
LuisFPradoSilva <luisfpradosilva.profissional@proton.me>
2024-11-07 15:38:01 -0300

    Criando carga de dados para testes

---
+++ src/main/java/com/mercadinho/services/DBService.java
@@ -0,0 +1,55 @@
+package com.mercadinho.services;
+
+import com.mercadinho.domains.Cliente;
+import com.mercadinho.domains.Funcionario;
+import com.mercadinho.domains.Produto;
+import com.mercadinho.domains.Venda;
+import com.mercadinho.repositories.ClienteRepository;
+import com.mercadinho.repositories.FuncionarioRepository;
+import com.mercadinho.repositories.ProdutoRepository;
+import com.mercadinho.repositories.VendaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DBService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @Autowired
+    private VendaRepository vendaRepository;
+
+    public void initDB() {
+
+        Cliente cliente1 = new Cliente("11122233344", "Luís Felipe Prado da Silva", 1L);
+        Cliente cliente2 = new Cliente("11111111111", "Luís Fagundes", 2L);
+        Funcionario funcionario1 = new Funcionario("77788899955", "Fernando Rocha", 3L);
+        Funcionario funcionario2 = new Funcionario("22222222222", "Fernanda Alcantra", 4L);
+        Venda venda1 = new Venda(null, 20.00, cliente1);
+        Venda venda2 = new Venda(null, 10.00, cliente2);
+        Produto produto1 = new Produto(1, "Arroz", "Saco de arroz de 1kg", 15.00, 20.00, "UN" ,"Alimentos", venda1);
+        Produto produto2 = new Produto(2, "Alface", "Caveça de alfaçe crespa", 8.00, 10.00, "UN" ,"Alimentos", venda2);
+
+        venda1.getProdutos().add(produto1);
+        venda2.getProdutos().add(produto2);
+
+        cliente1.getVendas().add(venda1);
+        cliente2.getVendas().add(venda2);
+
+        clienteRepository.save(cliente1);
+        clienteRepository.save(cliente2);
+        funcionarioRepository.save(funcionario1);
+        funcionarioRepository.save(funcionario2);
+        vendaRepository.save(venda1);
+        vendaRepository.save(venda2);
+        produtoRepository.save(produto1);
+        produtoRepository.save(produto2);
+    }
+}
