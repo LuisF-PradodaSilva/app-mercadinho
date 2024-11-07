930c063317065d80c3b2f5f725c82e22d31f49b6
LuisFPradoSilva <luisfpradosilva.profissional@proton.me>
2024-11-07 15:17:22 -0300

    Adicionando annotations @EntityScan, @ComponentScan e @EnableJpaRepositories na classe principal

--- src/main/java/com/mercadinho/appmercadinho/AppmercadinhoApplication.java
+++ src/main/java/com/mercadinho/appmercadinho/AppmercadinhoApplication.java
@@ -2,7 +2,13 @@ package com.mercadinho.appmercadinho;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EntityScan(basePackages = {"com.mercadinho.domains", "com.mercadinho.domains,enums"})
+@ComponentScan(basePackages = "com.mercadinho")
+@EnableJpaRepositories(basePackages = "com.mercadinho.repositories")
 @SpringBootApplication
 public class AppmercadinhoApplication {
 
