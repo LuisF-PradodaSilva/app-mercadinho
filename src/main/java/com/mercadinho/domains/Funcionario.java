d47ce60576b18a71ae484d601f82108453076f99
LuisFPradoSilva <luisfpradosilva.profissional@proton.me>
2024-11-07 12:06:55 -0300

    Finalizando programação das classes Cliente e Funcionario

--- src/main/java/com/mercadinho/domains/Funcionario.java
+++ src/main/java/com/mercadinho/domains/Funcionario.java
@@ -1,4 +1,16 @@
 package com.mercadinho.domains;
 
+import java.time.LocalDate;
+import java.util.Set;
+
 public class Funcionario extends Pessoa {
+
+    public Funcionario() {
+        super();
+    }
+
+    public Funcionario(Set<Integer> tipoPessoa, LocalDate dataCadastro, String cpf, String nomePessoa, Long id) {
+        super(tipoPessoa, dataCadastro, cpf, nomePessoa, id);
+    }
+    
 }
