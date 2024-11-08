76cd9d09a32c68851a829e415e8534118ff083eb
LuisFPradoSilva <luisfpradosilva.profissional@proton.me>
2024-11-08 09:41:15 -0300

    Implementando busca de cliente por ID

--- src/main/java/com/mercadinho/resources/ClienteResource.java
+++ src/main/java/com/mercadinho/resources/ClienteResource.java
@@ -1,10 +1,12 @@
 package com.mercadinho.resources;
 
+import com.mercadinho.domains.Cliente;
 import com.mercadinho.domains.dtos.ClienteDTO;
 import com.mercadinho.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,11 @@ public class ClienteResource {
     @GetMapping
     public ResponseEntity<List<ClienteDTO>> findAll() {
         return ResponseEntity.ok().body(clienteService.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClienteDTO> findById(@PathVariable Long id) {
+        Cliente obj = this.clienteService.findById(id);
+        return ResponseEntity.ok().body(new ClienteDTO(obj));
     }
 }
--- src/main/java/com/mercadinho/services/ClienteService.java
+++ src/main/java/com/mercadinho/services/ClienteService.java
@@ -1,11 +1,13 @@
 package com.mercadinho.services;
 
+import com.mercadinho.domains.Cliente;
 import com.mercadinho.domains.dtos.ClienteDTO;
 import com.mercadinho.repositories.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -16,5 +18,10 @@ public class ClienteService {
 
     public List<ClienteDTO> findAll() {
         return clienteRepository.findAll().stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+    }
+
+    public Cliente findById(Long id) {
+        Optional<Cliente> obj = clienteRepository.findById(id);
+        return obj.orElse(null);
     }
 }
