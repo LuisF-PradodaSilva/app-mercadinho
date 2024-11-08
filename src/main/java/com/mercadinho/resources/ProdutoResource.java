8743a76f4054e834251370fd38e31a8183202d8a
LuisFPradoSilva <luisfpradosilva.profissional@proton.me>
2024-11-08 14:54:39 -0300

    Implementando cadastro de produtos

--- src/main/java/com/mercadinho/resources/ProdutoResource.java
+++ src/main/java/com/mercadinho/resources/ProdutoResource.java
@@ -5,11 +5,10 @@ import com.mercadinho.domains.dtos.ProdutoDTO;
 import com.mercadinho.services.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,5 +27,12 @@ public class ProdutoResource {
     public ResponseEntity<ProdutoDTO> findById(@PathVariable int id) {
         Produto obj = this.produtoService.findById(id);
         return ResponseEntity.ok().body(new ProdutoDTO(obj));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProdutoDTO> create(@RequestBody ProdutoDTO objDto) {
+        Produto newObj = produtoService.create(objDto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/mercadinho/services/ProdutoService.java
+++ src/main/java/com/mercadinho/services/ProdutoService.java
@@ -1,8 +1,10 @@
 package com.mercadinho.services;
 
 import com.mercadinho.domains.Produto;
+import com.mercadinho.domains.Venda;
 import com.mercadinho.domains.dtos.ProdutoDTO;
 import com.mercadinho.repositories.ProdutoRepository;
+import com.mercadinho.repositories.VendaRepository;
 import com.mercadinho.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +19,9 @@ public class ProdutoService {
     @Autowired
     private ProdutoRepository produtoRepository;
 
+    @Autowired
+    private VendaService vendaService;
+
     public List<ProdutoDTO> findAll() {
         return produtoRepository.findAll().stream().map(obj -> new ProdutoDTO(obj)).collect(Collectors.toList());
     }
@@ -24,5 +29,29 @@ public class ProdutoService {
     public Produto findById(int id) {
         Optional<Produto> obj = produtoRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Produto não encontrado no sistema! ID: " + id));
+    }
+
+    private Produto newProduto(ProdutoDTO objDto) {
+        Venda venda = vendaService.findById(objDto.getVenda());
+
+        Produto produto = new Produto();
+
+        if (objDto.getId() != 0) {
+            produto.setId(objDto.getId());
+        }
+
+        produto.setVenda(venda);
+        produto.setNomeProduto(objDto.getNomeProduto());
+        produto.setDescricao(objDto.getDescricao());
+        produto.setValorCompra(objDto.getValorCompra());
+        produto.setValorVenda(objDto.getValorVenda());
+        produto.setUnidadeMedida(objDto.getUnidadeMedida());
+        produto.setTipoProduto(objDto.getTipoProduto());
+
+        return produto;
+    }
+
+    public Produto create(ProdutoDTO ProdutoDto) {
+        return produtoRepository.save(newProduto(ProdutoDto));
     }
 }
