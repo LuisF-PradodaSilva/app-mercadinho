7de4aa2feba5b741e88ef38e293e0361cf7f5a1a
LuisFPradoSilva <luisfpradosilva.profissional@proton.me>
2024-11-08 09:05:26 -0300

    Finalizando implementação do findAll() para Produto

---
+++ src/main/java/com/mercadinho/resources/ProdutoResource.java
@@ -0,0 +1,24 @@
+package com.mercadinho.resources;
+
+import com.mercadinho.domains.dtos.ProdutoDTO;
+import com.mercadinho.services.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/produto")
+public class ProdutoResource {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @GetMapping
+    public ResponseEntity<List<ProdutoDTO>> findAll() {
+        return ResponseEntity.ok().body(produtoService.findAll());
+    }
+}
