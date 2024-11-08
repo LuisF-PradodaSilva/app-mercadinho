322a52ea51616574988e8d90dd127eeab62e0135
LuisFPradoSilva <luisfpradosilva.profissional@proton.me>
2024-11-08 08:38:59 -0300

    Criando a classe ProdutoService

---
+++ src/main/java/com/mercadinho/services/ProdutoService.java
@@ -0,0 +1,18 @@
+package com.mercadinho.services;
+
+import com.mercadinho.domains.dtos.ProdutoDTO;
+import com.mercadinho.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public List<ProdutoDTO> findAll() {
+        return produtoRepository.findAll().stream().map(obj -> new ProdutoDTO(obj)).collect(Collectors.toList());
+    }
+}
