024d671d8a960d3a57eba1770152f57e7d352d7e
LuisFPradoSilva <luisfpradosilva.profissional@proton.me>
2024-11-08 08:31:51 -0300

    Corrigindo erro no ClienteRepository

--- src/main/java/com/mercadinho/repositories/ClienteRepository.java
+++ src/main/java/com/mercadinho/repositories/ClienteRepository.java
@@ -1,10 +1,10 @@
 package com.mercadinho.repositories;
 
-import com.mercadinho.domains.Pessoa;
+import com.mercadinho.domains.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClienteRepository extends JpaRepository<Pessoa, Long> {
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
 }
