115fbaeec236f24a1ac987854059d9d8ba450bee
LuisFPradoSilva <luisfpradosilva.profissional@proton.me>
2024-11-11 13:13:49 -0300

    Criando a classe UserDetailsServiceImpl

---
+++ src/main/java/com/mercadinho/services/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.mercadinho.services;
+
+import com.mercadinho.domains.Funcionario;
+import com.mercadinho.repositories.FuncionarioRepository;
+import com.mercadinho.security.UserSS;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final FuncionarioRepository funcionarioRepository;
+
+    public UserDetailsServiceImpl(FuncionarioRepository funcionarioRepository) {
+        this.funcionarioRepository = funcionarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Funcionario> funcionario = funcionarioRepository.findByCpf(username);
+
+        if (funcionario.isEmpty()) {
+            throw new UsernameNotFoundException("User not found! CPF: " + username);
+        }
+        return new UserSS(funcionario.orElse(null));
+    }
+}
