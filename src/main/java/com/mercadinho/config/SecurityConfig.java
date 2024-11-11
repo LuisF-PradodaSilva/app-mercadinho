5c7ab7e9b1b254bb9077c62169c2725faf36bf85
LuisFPradoSilva <luisfpradosilva.profissional@proton.me>
2024-11-11 14:35:15 -0300

    Adicionando @Service em UserDetailsServiceImpl e removendo importaões não usadas em SecurityConfig

--- src/main/java/com/mercadinho/config/SecurityConfig.java
+++ src/main/java/com/mercadinho/config/SecurityConfig.java
@@ -11,8 +11,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -77,5 +75,5 @@ public class SecurityConfig {
         authenticationManagerBuilder.userDetailsService(userDetailsService);
         return authenticationManagerBuilder.build();
     }
-    
+
 }
